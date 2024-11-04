3cbcc23f8719bca6ab05f35755d2af63f9dfaf1b
Abd Elbasset Khettabi <abdelbassetkhettabi@gmail.com>
2024-11-04 23:27:45 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,50 @@
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import FAQ from './components/FAQ';
-import Newsletter from './components/Newsletter';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import QuestionForm from './components/QuestionForm';
+"use client"
+
+import { useState } from 'react'
+import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import FAQ from './components/FAQ'
+import Newsletter from './components/Newsletter'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import QuestionForm from './components/QuestionForm'
+import ProgramDetails from './components/ProgramDetails'
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState('home');
+  const [currentPage, setCurrentPage] = useState('home')
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'login':
+        return <Login />
+      case 'question':
+        return <QuestionForm />
+      case 'program':
+        return <ProgramDetails />
+      default:
+        return (
+          <>
+            <Hero />
+            <Features />
+            <ProgramDetails />
+            <FAQ onNavigate={setCurrentPage} />
+            <Newsletter />
+          </>
+        )
+    }
+  }
 
   return (
-    <div className="bg-black min-h-screen">
-      <Navbar onNavigate={setCurrentPage} />
-      {currentPage === 'login' ? (
-        <Login />
-      ) : currentPage === 'question' ? (
-        <QuestionForm />
-      ) : (
-        <>
-          <Hero />
-          <Features />
-          <FAQ onNavigate={setCurrentPage} />
-          <Newsletter />
-          <Footer />
-        </>
-      )}
-    </div>
-  );
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="bg-background min-h-screen">
+        <Navbar onNavigate={setCurrentPage} />
+        <main>
+          {renderPage()}
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
 }
